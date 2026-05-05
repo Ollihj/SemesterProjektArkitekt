7e1fb95c7bd1eaa2dfd243973a25b0ee290e7768
Ollihj <eaa24ohj@students.eaaa.dk>
2026-05-05 12:55:49 +0200

    Clean up - remove services and pages, keep Core models and project skeleton

--- ArchitektApp/Program.cs
+++ ArchitektApp/Program.cs
@@ -1,22 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ArchitektApp;
-using ArchitektApp.Pages;
-using ArchitektApp.Service;
 using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Config.ServerUrl) });
-builder.Services.AddScoped<IBrugerService, BrugerServiceHttp>();
-builder.Services.AddScoped<IBookingService, BookingServiceHttp>();
-builder.Services.AddScoped<IAftaleService, AftaleServiceHttp>();
-builder.Services.AddScoped<IBlokeretService, BlokeretServiceHttp>();
-builder.Services.AddScoped<IProjektService, ProjektServiceHttp>();
-builder.Services.AddScoped<LoginService>();
-builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -1,19 +1,7 @@
-using MongoDB.Bson.Serialization.Conventions;
-using ServerAPI.Repositories;
-
-var conventionPack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
-ConventionRegistry.Register("IgnoreExtraElements", conventionPack, _ => true);
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-builder.Services.AddSingleton<IBrugerRepository, BrugerRepositoryMongoDB>();
-builder.Services.AddSingleton<IBookingRepository, BookingRepositoryMongoDB>();
-builder.Services.AddSingleton<IAftaleRepository, AftaleRepositoryMongoDB>();
-builder.Services.AddSingleton<IBlokeretRepository, BlokeretRepositoryMongoDB>();
-builder.Services.AddSingleton<IProjektRepository, ProjektRepositoryMongoDB>();
 
 builder.Services.AddCors(options =>
 {
